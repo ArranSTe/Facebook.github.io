1101ac32773c15cb775b9fedd16a9913d54de3fd
ArranSTe <165517244+ArranSTe@users.noreply.github.com>
2025-04-17 13:16:10 +0100

    Update info.js

--- info.js
+++ info.js
@@ -1,7 +1,3 @@
-let username;
 
-document.getElementById("log").onclick = function(){
-    username = document.getElementById("email").value;
-    document.getElementById("9876").textContent = `${username}`
-    console.log(username)
-}
+
+document.getElementById("
