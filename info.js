a8a370dc5b61735a5f80a1a2b3aee0fb0a93e189
ArranSTe <165517244+ArranSTe@users.noreply.github.com>
2025-04-17 13:09:42 +0100

    Update info.js

--- info.js
+++ info.js
@@ -1,15 +1,7 @@
 let username;
-let password;
 
 document.getElementById("log").onclick = function(){
     username = document.getElementById("email").value;
     document.getElementById("9876").textContent = `${username}`
     console.log(username)
 }
-
-document.getElementById('log').onclick = function(){
-    password = document.getElementById('Password').value;
-    document.getElementById('1234').textContent = `${password}`
-    console.log(password)
-}
-
