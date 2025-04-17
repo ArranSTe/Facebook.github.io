3a453532587596d3ca5baf7d6c799b3ebf633aa9
ArranSTe <165517244+ArranSTe@users.noreply.github.com>
2025-04-17 13:01:00 +0100

    Update info.js

--- info.js
+++ info.js
@@ -1,6 +1,11 @@
 let username;
+let password;
 
 document.getElementById("log").onclick = function(){
     username = document.getElementById("email").value;
+    password = document.getElementById("password").value;
     document.getElementById("9876").textContent = `${username}`
+    document.getElementById("9876").textContent = `${password}`
+    console.log(username)
+    console.log(password)
 }
